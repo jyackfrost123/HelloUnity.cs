9262fb679f9a851f00175ab0ab3fd3635d3d65fc
jyackfrost123 <33057576+jyackfrost123@users.noreply.github.com>
2021-06-09 08:18:40 +0900

    some fix HelloUnity.js

--- Assets/Scripts/HelloUnity.js
+++ Assets/Scripts/HelloUnity.js
@@ -1,3 +1,3 @@
 //「HelloUnity.js」は、unity-chan!公式HP(URL: https://unity-chan.com/)で公式配布中！
-//もし元の歌詞が気になる方や音源(.mp3, .wav)が欲しいみんなは、ぜひ『DATA DOWNLOAD』から、
+//もし元の歌詞が気になる方や音源(.mp3, .wav)が欲しいみんなは、『DATA DOWNLOAD』から、
 //『キャラクターソング・アルバム Vol.1「UNITE IN THE SKY」』をダウンロードしてみてね！
